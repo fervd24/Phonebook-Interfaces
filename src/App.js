cfd2f7cc81b9ca46b12c7c342eedbb25eb6617b4
fernando villalobos <fvd_24_93@hotmail.com>
2022-08-08 11:55:40 -0500

    Organizing the files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { 
+  Routes,
+  Route
+} from 'react-router-dom';
+import Home from './components/home/Home';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>      
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+    </>
   );
 }
 
